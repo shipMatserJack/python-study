f364f268043a1aafe9065edadfa9a25f77715c9e
黄杰 <841440416@qq.com>
2026-03-05 23:13:13 +0800

    python 基础语法

---
+++ 语法.py
@@ -0,0 +1,43 @@
+# 单行注释
+
+"""
+多行注释
+1111
+"""
+
+# 1. 打印
+print("hello world")
+
+# 2. 变量
+name = "张三"
+age = 18
+print("name:", name, "age:", age)
+
+# 3. 数据类型
+# 3.1 整数
+num = 10
+print("num:", num)
+# 3.2 浮点数
+float_num = 10.5
+print("float_num:", float_num)
+# 3.3 字符串
+string = "hello"
+print("string:", string)
+# 3.4 布尔值
+bool_value = True
+print("bool_value:", bool_value)
+# 3.5 空值
+none_vale = None
+print("none_vale:", none_vale)
+# 3.6 列表
+list = [1, 2, 3, 4, 5]
+print("list:", list)
+# 3.7 元组
+tuple = (1, 2, 3, 4, 5)
+print("tuple:", tuple)
+# 3.8 字典
+dict = {"name": "张三", "age": 18}
+print("dict:", dict)
+# 3.9 集合
+my_set = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print("my_set:", my_set)
