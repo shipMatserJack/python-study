eba9184dc0862feb6828ce5525a2ba689139810d
黄杰 <841440416@qq.com>
2026-03-09 23:05:20 +0800

    元祖

---
+++ 元组.py
@@ -0,0 +1,6 @@
+# https://docs.python.org/zh-cn/3/tutorial/datastructures.html#tuples-and-sequences
+my_tuple = (1, 2, 3, 4, 5)
+# my_tuple[0] = 10 # 元组是不可变的，不能修改元素
+my_tuple = (1, 2, 3, 4, 5, 6) # 重新赋值
+my_tuple = '1', (2, 3, 4, 5, 6) # 元组可以包含其他元组
+print(my_tuple)
